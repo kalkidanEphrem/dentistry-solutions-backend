573231c9f1c063af49a000e6c2908a94c1a240a7
Brook Alemu <brookwworku3@gmail.com>
2024-02-05 10:26:18 -0900

    adding fixes to blogs api

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import bodyParser from "body-parser";
 import express from "express";
-
+import { parseRequestBody } from "./middleware/parseRequest";
+import blogRouter from "./route/blogsRoute";
 
 const app = express();
 const PORT = 3000;
 
+app.use(parseRequestBody);
+app.use("/blogs", blogRouter)
 
 app.listen(PORT, () => {
-  console.log("Server is runing");
+  console.log("Server is runing on port" + PORT);
 });
-
-
-
-app.use(bodyParser.json());
 
 
--- src/controller/blogsController.ts
+++ src/controller/blogsController.ts
@@ -1,67 +1,64 @@
 import { Request, Response } from "express";
-import {sql} from '@vercel/postgres'
+import { sql } from "@vercel/postgres";
 
 class BlogController {
-    async getAllBlogs(req: Request, res: Response) {
-        try {
-            const blogs = await sql `SELECT * FROM blogs`;
-            return res.status(200).json(blogs)
-        
-        } catch (error) {
-              console.error(error)
-              return res.status(500).send('Internal Server Error')  
-        }     
+  async getAllBlogs(req: Request, res: Response) {
+    try {
+      const blogs = await sql`SELECT * FROM blogs`;
+      return res.status(200).json(blogs);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
     }
+  }
 
-    async getBlogById(req: Request, res: Response) {
-        const {id} = req.params
+  async getBlogById(req: Request, res: Response) {
+    const { id } = req.params;
 
-        try {
-            const blog = await sql `SELECT * FROM blogs WHERE id = ${id}`
-            return res.status(200).json(blog)
-        } catch (error) {
-            console.error(error)
-            return res.status(404).send('Blog not found')
-        }
+    try {
+      const blog = await sql`SELECT * FROM blogs WHERE id = ${id}`;
+      return res.status(200).json(blog);
+    } catch (error) {
+      console.error(error);
+      return res.status(404).send("Blog not found");
     }
+  }
 
+  async createBlog(req: Request, res: Response) {
+    const { title, discription, content, author } = req.body;
 
-    async createBlog(req: Request, res: Response) {
-        const {title, discription, content, author } = req.body;
-
-        try {
-            const newBlog = await sql`INSERT INTO blogs (title, description, content, author) VALUES (${title}, ${discription}, ${content}, ${author}) RETURNING *`;
-            return res.status(201).json(newBlog);
-          } catch (error) {
-            console.error(error);
-            return res.status(500).send('Internal Server Error');
-          }
+    try {
+      await sql`INSERT INTO blogs (title, description, content, author) VALUES (${title}, ${discription}, ${content}, ${author}) RETURNING *`;
+      const newBlog = { title, discription, content, author };
+      return res.status(201).json(newBlog);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
     }
+  }
 
-    async updateBlog(req: Request, res: Response) {
-        const { id } = req.params;
-        const { title, content } = req.body;
-        try {
-          const updatedBlog = await sql`UPDATE blogs SET title = $1, content = $2 WHERE id = $3 RETURNING *, (${title}, ${content}, ${id})`;
-          return res.status(200).json(updatedBlog)
-        } catch (error) {
-          console.error(error);
-          return res.status(404).send('Blog not found');
-        }
-      }
-    
-      async deleteBlog(req: Request, res: Response) {
-        const { id } = req.params;
-        try {
-          await sql `DELETE FROM blogs WHERE id = ${id}`;
-          return res.status(204).end();
-        } catch (error) {
-          console.error(error);
-          return res.status(404).send('Blog not found');
-        }
-      }
+  async updateBlog(req: Request, res: Response) {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    try {
+      await sql`UPDATE blogs SET title = $1, content = $2 WHERE id = $3 RETURNING *, (${title}, ${content}, ${id})`;
+      return res.status(200).send("Blog update successfully");
+    } catch (error) {
+      console.error(error);
+      return res.status(404).send("Blog not found");
+    }
+  }
 
-
+  async deleteBlog(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      await sql`DELETE FROM blogs WHERE id = ${id}`;
+      return res.status(204).end();
+    } catch (error) {
+      console.error(error);
+      return res.status(404).send("Blog not found");
+    }
+  }
 }
 
 export default new BlogController();
